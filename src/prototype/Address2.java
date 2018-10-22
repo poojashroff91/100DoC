69cc8c5c594de13dc2d5b485e7610160266bfc38
poshroff <poshroff@paypal.com>
2018-10-21 20:49:44 -0700

    Copy using copy constructor.

---
+++ src/prototype/Address2.java
@@ -0,0 +1,59 @@
+package prototype;
+
+public class Address2 {
+
+    public String streetAddress, city, country;
+
+    public Address2(String streetAddress, String city, String country) {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.country = country;
+    }
+    public Address2(Address2 other){
+        this(other.streetAddress, other.city, other.country);
+    }
+
+    @Override
+    public String toString() {
+        return "Address2{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
+
+class Employee {
+    public String name;
+    public Address2 address;
+
+    public Employee(String name, Address2 address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+
+    public Employee(Employee other) {
+        name = other.name;
+        address = new Address2(other.address);
+    }
+}
+
+
+class CopyConstructorDemo {
+    public static void main(String[] args) {
+        Employee john = new Employee("John", new Address2("123 London Road", "London", "UK"));
+        Employee chris = new Employee(john);
+
+        chris.name = "Chris";
+        System.out.println(john);
+        System.out.println(chris);
+    }
+}
